4d64e60b88669f538730d167f461927497ea8a61
Sean Flanigan <sflaniga@redhat.com>
2017-06-23 03:27:55 +1000

    Convert IntegrationTest to use coroutines

--- client/src/test/java/org/flanigan/proxyhook/client/IntegrationTest.kt
+++ client/src/test/java/org/flanigan/proxyhook/client/IntegrationTest.kt
@@ -1,21 +1,22 @@
 package org.flanigan.proxyhook.client
 
-import io.vertx.core.CompositeFuture
+import io.vertx.core.AsyncResult
 import io.vertx.core.Future
+import io.vertx.core.Handler
 import io.vertx.core.Vertx
+import io.vertx.core.http.HttpServer
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.unit.junit.VertxUnitRunner
+import kotlinx.coroutines.experimental.runBlocking
 import org.asynchttpclient.DefaultAsyncHttpClient
 import org.flanigan.proxyhook.server.ProxyHookServer
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.net.ServerSocket
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit.MILLISECONDS
 
-@Suppress("INTERFACE_STATIC_METHOD_CALL_FROM_JAVA6_TARGET")
 @RunWith(VertxUnitRunner::class)
 class IntegrationTest {
 
@@ -49,76 +50,83 @@ class IntegrationTest {
     }
 
     @Test
-    fun deliverProxiedWebhook() {
+    fun deliverProxiedWebhook(): Unit = runBlocking {
         // this future will succeed if the test passes,
         // or fail if something goes wrong.
         val testFinished = CompletableFuture<Unit>()
 
-        val serverPort = Future.future<Int>()
-        server.deployVerticle(ProxyHookServer(0, serverPort))
+        val (_, serverPort) = startServer()
 
-        val webhookPort = Future.future<Int>()
-        webhook.createHttpServer().requestHandler { req ->
-            req.response().statusCode = 200
-            req.response().end()
+        val webhookPort = createWebhookReceiver(testFinished)
+        val receiveUrl = "http://localhost:${webhookPort.await()}/"
+        val websocketUrl = "ws://localhost:${serverPort.await()}/listen"
+        val postUrl = "http://localhost:${serverPort.await()}/webhook"
+        // wait for proxyhook server and webhook receiver before starting client
+        val client = startClient(testFinished, websocketUrl, receiveUrl)
 
-            req.bodyHandler { buffer ->
-                val payload = buffer.toString(Charsets.UTF_8)
-                if (payload == webhookPayload) {
-                    // we received the proxy webhook with its payload, so the test has passed:
-                    testFinished.complete(Unit)
-                } else {
-                    testFinished.completeExceptionally(AssertionError("wrong payload: $payload"))
-                }
-            }
-        }.listen(0) { // listen on random port
-            if (it.failed()) {
-                webhookPort.fail(it.cause())
-            } else {
-                val actualPort = it.result().actualPort()
-                log.info("webhook receiver ready on port $actualPort")
-                webhookPort.complete(actualPort)
-            }
-        }
+        // wait for client login before sending webhook to proxyhook server
+        client.await()
+        log.info("client ready, preparing to POST webhook")
 
-        CompositeFuture.all(serverPort, webhookPort).compose<Unit> {
-            // wait for proxyhook server and webhook receiver before starting client
-            if (it.failed()) testFinished.completeExceptionally(it.cause())
-            val websocketUrl = "ws://localhost:${serverPort.result()}/listen"
-            val receiveUrl = "http://localhost:${webhookPort.result()}/"
-
-            log.info("deploying client")
-            val clientReady = Future.future<Unit>()
-            client.deployVerticle(ProxyHookClient(clientReady, websocketUrl, receiveUrl)) {
-                if (it.failed()) {
-                    testFinished.completeExceptionally(it.cause())
-                }
-            }
-            clientReady
-        }.setHandler {
-            // wait for client login before sending webhook to proxyhook server
-            if (it.failed()) {
-                testFinished.completeExceptionally(it.cause())
-            } else {
-                log.info("client ready, preparing to POST webhook")
-                val postUrl = "http://localhost:${serverPort.result()}/webhook"
-
-                val httpClient = DefaultAsyncHttpClient()
-                httpClient.preparePost(postUrl)
-                        .setBody(webhookPayload)
-                        .execute()
-                        .toCompletableFuture()
-                        .whenComplete { response, e ->
-                            if (e != null) {
-                                testFinished.completeExceptionally(e)
-                            } else if (response != null && response.statusCode != 200) {
-                                testFinished.completeExceptionally(AssertionError("bad response ${response.statusCode}: ${response.responseBody}"))
-                            }
-                        }
-            }
-        }
+        postWebhookToServer(postUrl, testFinished)
 
         testFinished.get(TEST_TIMEOUT_MS, MILLISECONDS)
+    }
+
+    private suspend fun startServer(): Pair<Future<String>, Future<Int>> {
+        val actualPort = Future.future<Int>()
+        val deploymentId = futureVx<String> { it: Handler<AsyncResult<String>> ->
+            server.deployVerticle(ProxyHookServer(0, actualPort), it)
+        }
+        return deploymentId to actualPort
+    }
+
+    private fun createWebhookReceiver(testFinished: CompletableFuture<Unit>): Future<Int> = future {
+        val httpServer: HttpServer = vx {
+            webhook.createHttpServer().requestHandler { req ->
+                req.response().statusCode = 200
+                req.response().end()
+
+                req.bodyHandler { buffer ->
+                    val payload = buffer.toString(Charsets.UTF_8)
+                    if (payload == webhookPayload) {
+                        // we received the proxy webhook with its payload, so the test has passed:
+                        testFinished.complete(Unit)
+                    } else {
+                        testFinished.completeExceptionally(AssertionError("wrong payload: $payload"))
+                    }
+                }
+            }.listen(0, it) // listen on random port
+        }
+        val actualPort = httpServer.actualPort()
+        log.info("webhook receiver ready on port $actualPort")
+        actualPort
+    }
+
+    private fun startClient(testFinished: CompletableFuture<Unit>, websocketUrl: String, receiveUrl: String): Future<Unit> {
+        log.info("deploying client")
+        val clientReady = Future.future<Unit>()
+        client.deployVerticle(ProxyHookClient(clientReady, websocketUrl, receiveUrl)) {
+            if (it.failed()) {
+                testFinished.completeExceptionally(it.cause())
+            }
+        }
+        return clientReady
+    }
+
+    private fun postWebhookToServer(postUrl: String, testFinished: CompletableFuture<Unit>) {
+        val httpClient = DefaultAsyncHttpClient()
+        httpClient.preparePost(postUrl)
+                .setBody(webhookPayload)
+                .execute()
+                .toCompletableFuture()
+                .whenComplete { response, e ->
+                    if (e != null) {
+                        testFinished.completeExceptionally(e)
+                    } else if (response != null && response.statusCode != 200) {
+                        testFinished.completeExceptionally(AssertionError("bad response ${response.statusCode}: ${response.responseBody}"))
+                    }
+                }
     }
 
 }
---
+++ client/src/test/java/org/flanigan/proxyhook/client/coroutines.kt
@@ -0,0 +1,48 @@
+package org.flanigan.proxyhook.client
+
+import io.vertx.core.AsyncResult
+import io.vertx.core.Future
+import io.vertx.core.Handler
+import kotlinx.coroutines.experimental.CommonPool
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.experimental.suspendCoroutine
+
+/**
+ * @author Sean Flanigan <a href="mailto:sflaniga@redhat.com">sflaniga@redhat.com</a>
+ */
+
+// https://github.com/Kotlin/kotlin-coroutines/blob/6833648d6d3ddbaf7b31511a635f11fd6c7a3115/kotlin-coroutines-informal.md#wrapping-callbacks
+inline suspend fun <T> vx(crossinline callback: (Handler<AsyncResult<T>>) -> Unit) =
+        suspendCoroutine<T> { cont ->
+            callback(Handler { result: AsyncResult<T> ->
+                if (result.succeeded()) {
+                    cont.resume(result.result())
+                } else {
+                    cont.resumeWithException(result.cause())
+                }
+            })
+        }
+
+fun <T> future(context: CoroutineContext = CommonPool, block: suspend () -> T): Future<T> =
+        FutureCoroutine<T>(context, Future.future()).also { block.startCoroutine(completion = it) }
+
+fun <T> futureVx(callback: (Handler<AsyncResult<T>>) -> Unit): Future<T> = future {
+    vx<T> {
+        callback(it)
+    }
+}
+
+private class FutureCoroutine<T>(override val context: CoroutineContext, f: Future<T>) : Future<T> by f, Continuation<T> {
+    override fun resume(value: T) { complete(value) }
+    override fun resumeWithException(exception: Throwable) { fail(exception) }
+}
+
+suspend fun <T> Future<T>.await(): T =
+        suspendCoroutine<T> { cont: Continuation<T> ->
+            setHandler { future ->
+                if (future.succeeded()) cont.resume(future.result())
+                else cont.resumeWithException(future.cause())
+            }
+        }
