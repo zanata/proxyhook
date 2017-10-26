70201015ed9b2afd04422394e52fb00eb9fdb4f8
Ding-Yi Chen <dchen@redhat.com>
2017-10-26 16:35:31 +1000

    Set pipeline-library to v0.3.0

--- pipeline/src/test/java/org/flanigan/proxyhook/pipeline/TestJenkinsfile.kt
+++ pipeline/src/test/java/org/flanigan/proxyhook/pipeline/TestJenkinsfile.kt
@@ -34,7 +34,7 @@ class TestJenkinsfile : BasePipelineTestCPS() {
                 // uncomment to use already-downloaded (perhaps modified) copy instead of git:
                 //                .retriever(localSource(LIB_PATH))
                 .targetPath(LIB_PATH)
-                .defaultVersion("ZNTA-2234-tag")
+                .defaultVersion("master")
                 .allowOverride(true)
                 .implicit(false)
                 .build()
